db76bf264179401b63d4e3432846305a0b08ea01
Elian <eliansa123@gmail.com>
2024-01-17 12:44:02 -0500

    Inicio del proyecto y creación de base de datos

---
+++ database/factories/TransaccionFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaccion>
+ */
+class TransaccionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            // 'monto' => $this->faker->numerify('######'),
+            // 'categoria_id' => $this->faker->numerify('######'),
+            // 'user_id' => $this->faker->numerify('######'),
+            // 'fecha' => $this->faker->numerify('######'),
+            // 'tipo' => $this->faker->numerify('######'),
+            // 'descripcion' => $this->faker->numerify('######')
+
+        ];
+    }
+}
