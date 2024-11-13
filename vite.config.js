154af2f7b91ce676ba26099971a5b0c2f1834862
Elian <eliansa123@gmail.com>
2024-11-12 19:35:44 -0500

    Se añaden dependencias para compilacion

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import laravel from 'laravel-vite-plugin';
  
 export default defineConfig({
     plugins: [
-        laravel(['resources/js/app.js']),
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
     ],
     resolve: {
         alias: {
